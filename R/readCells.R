2afe1262039d40eb0d6f720593325387302f0635
Bogdan Oancea <bogdan.oancea@gmail.com>
2020-06-03 19:49:25 +0300

    bug fixes

--- R/readCells.R
+++ R/readCells.R
@@ -16,6 +16,9 @@
 #   }
 #   close(con)
 # }
+#file<-'/home/bogdan/r-projects/vis-david/dataset5/AntennaCells_Vodafone.csv'
+#file<-'/home/bogdan/r-projects/vis-david2/dataset1b/AntennaCells_Vodafone.csv'
+
 
 readCells <- function (cellsFileName) {
   coverArea <- fread(cellsFileName, sep ='\n', header = TRUE, stringsAsFactors = FALSE)
@@ -29,11 +32,21 @@ readCells <- function (cellsFileName) {
     return(polygon)
   })
   coverArea <-coverArea[,-2]
-  y<-data.table(t(combn(antennas,2)))
+  
+  y <-data.table(t(combn(antennas,2)))
   y1<-merge(y, coverArea, by.x='V1', by.y='antennaID', all.x = TRUE, sort = TRUE)
+  rm(y)
   y2<-merge(y1, coverArea, by.x='V2', by.y='antennaID', all.x = TRUE, sort = TRUE)
+  rm(y1)
   colnames(y2)<-c('antennaID2', 'antennaID1', 'WKT1', 'WKT2')
-  
-  return (y2)
-  
+  res<-vector(length = nrow(y2))
+  for(i in 1:nrow(y2)) {
+     p1<-y2[[i,'WKT1']]
+     p2<-y2[[i,'WKT2']]
+     res[i]<-gIntersects(p1,p2)
+  }
+  y2[,'neighbour']<-res
+  return (y2[,.(antennaID1, antennaID2, neighbour)])
 }
+
+
