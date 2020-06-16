87df4c99db0cd3b0f5e2c2c6ca75b053cf1af25a
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2020-06-16 14:52:28 +0300

    getEmissionProbs - comments and params check

--- R/getEmissionProbs.R
+++ R/getEmissionProbs.R
@@ -1,61 +1,106 @@
-#' @title Builds the emission probabilities for the HMM used to estimate the prosterior location probabilitities.
-#' 
-#' 
-#' @description Builds the emission probabilities needed for the HMM used to estimate the posterior location probabilitities. 
+#' @title Builds the emission probabilities for the HMM used to estimate the posterior location probabilitities.
+#'
+#'
+#' @description Builds the emission probabilities needed for the HMM used to estimate the posterior location
+#' probabilitities.
 #' In case of using simulated data, these probabilities are build using the signal strength or signal quality outputed
 #' by the simulation software for each tile in the grid.
-#' 
-#' @return Returns a matrix with the emmission probabilitiews for the HMM. The number of rows equals the numberof
-#' tiles in the grid and the number of columns equals the number of antennas.
-#' 
-#' 
+#'
+#' @return Returns a Matrix object with the emmission probabilities for the HMM. The number of rows equals the
+#' number of tiles in the grid and the number of columns equals the number of antennas. An element (i,j) of
+#' this matrix coresponds to the probability of a device being in tile i to be connected to antena j. The row
+#' names of the matrix are the tile indices and the column names are the antenna IDs.
+#'
+#'@param nrow the number of rows in the grid. It can be obtained by calling \code{readGridParams}.
+#'
+#'@param ncol the number of columns in the grid. It can be obtained by calling \code{readGridParams}.
+#'
+#'@param signalFileName The name of the .csv file that contains the signal strength/quality for each tile in the grid.
+#'This file is one of the outputs of the data simulator. The data is organized as a matrix with the number of rows
+#'equals to the number of antennas and the the following columns: \code{Antenna ID, Tile 0, Tile 1, ... Tile (N-1)}.
+#'On the first column there are the antenna IDs and on the rest of the columns the coresponding signal 
+#'strength/quality.
+#'
+#'
+#'@param sigMin The minimum value of the signal strength/quality that allow a connection between a device and an
+#'antenna.
+#'
+#'@param handoverType The handover mechanism used by the mobile network. It could have two values:'strenght' or
+#''quality'
+#'
+#'@param simulatedData If TRUE, the input data provided to this function come from tha simulator otherwise the data 
+#'come from a real mobile network.
+#'
 #' @import data.table
 #' @import stringr
 #' @import Matrix
 #' @include tileEquivalence.R
-#' 
+#'
 #' @export
-getEmissionProbs <- function(nrows, ncols, signalFileName, sigMin, handoverType = 'strength', simulatedData = TRUE) {
-  if(simulatedData) {
-    if(!file.exists(signalFileName))
-      stop("The file with signal values doesn't exist")
-    
-    tileEquiv.dt <- data.table(tileEquivalence(nrows, ncols))
-    
-    
-    RSS <- fread(signalFileName, sep = ",", header = TRUE, stringsAsFactors = FALSE)
-    nTiles1 <- dim(RSS)[2] - 1
-    nTiles2 <- nrows * ncols
-    if (nTiles1 != nTiles2) {
-      stop("Number of rows and columns provided inconsistent with the signal file")
-    }
-    
-    setnames(RSS, c('antennaID', 0:(nTiles1 - 1)))
-    RSS <- melt(RSS, id.vars = 'antennaID', variable.name = 'tile', variable.factor = FALSE, value.name = 'RSS')
-    RSS[ , RSS := ifelse(RSS < sigMin, NA, RSS)]
-    if (handoverType == 'strength') {
-      # The radio wave model is expressed in log10. It seems natural to recover the original scale
-      RSS <- RSS[, eventLoc := 10**RSS / sum(10**RSS, na.rm = TRUE), by = 'tile']
+getEmissionProbs <-
+  function(nrows,
+           ncols,
+           signalFileName,
+           sigMin,
+           handoverType = 'strength',
+           simulatedData = TRUE) {
+
+    if (simulatedData == TRUE) {
+      if (!file.exists(signalFileName))
+        stop(paste0(signalFileName, " doesn't exist!"))
       
-    }  
-    # Make eventLoc=0 if the tile is out the coverage area
-    RSS <- RSS[is.na(eventLoc), eventLoc := 0]
-    RSS[ , tile := as.numeric(tile)]
-    
-    RSS <- RSS[tileEquiv.dt, on = 'tile'][  , tile := NULL]
-    
-    RSS <- dcast(RSS, rasterCell ~ antennaID, value.var = 'eventLoc')[, rasterCell := NULL]
-    
-    emissionProbs <- Matrix(data=as.matrix(RSS))
-    remove(RSS)
-    dimnames(emissionProbs)[[1]] <- as.character(1:dim(emissionProbs)[1])
-    
-    return (emissionProbs)
+      tileEquiv.dt <- data.table(tileEquivalence(nrows, ncols))
+      
+      
+      RSS <-
+        fread(
+          signalFileName,
+          sep = ",",
+          header = TRUE,
+          stringsAsFactors = FALSE
+        )
+      nTiles1 <- dim(RSS)[2] - 1
+      nTiles2 <- nrows * ncols
+      if (nTiles1 != nTiles2) {
+        stop("Number of rows and columns provided inconsistent with the signal file")
+      }
+      
+      setnames(RSS, c('antennaID', 0:(nTiles1 - 1)))
+      RSS <-
+        melt(
+          RSS,
+          id.vars = 'antennaID',
+          variable.name = 'tile',
+          variable.factor = FALSE,
+          value.name = 'RSS'
+        )
+      
+      RSS[, RSS := ifelse(RSS < sigMin, NA, RSS)]
+      if (handoverType == 'strength') {
+        # The radio wave model is expressed in log10. It seems natural to recover the original scale
+        RSS <-
+          RSS[, eventLoc := 10 ** RSS / sum(10 ** RSS, na.rm = TRUE), by = 'tile']
+        
+      }
+      # Make eventLoc=0 if the tile is out the coverage area
+      RSS <- RSS[is.na(eventLoc), eventLoc := 0]
+      RSS[, tile := as.numeric(tile)]
+      
+      RSS <- RSS[tileEquiv.dt, on = 'tile'][, tile := NULL]
+      
+      RSS <-
+        dcast(RSS, rasterCell ~ antennaID, value.var = 'eventLoc')[, rasterCell := NULL]
+      
+      emissionProbs <- Matrix(data = as.matrix(RSS))
+      remove(RSS)
+      
+      dimnames(emissionProbs)[[1]] <-
+        as.character(1:dim(emissionProbs)[1])
+      
+      return (emissionProbs)
+    }
+    else {
+      cat("Can't read real mobile network signal file yet!")
+      return (NULL)
+    }
   }
-  else {
-    cat("Can't read real mobile network signal file yet!")
-    return (NULL)
-  }
-}
-
-
