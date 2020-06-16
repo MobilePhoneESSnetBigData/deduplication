064c2497bb4efff03d5087249ad9dfe0af4e5d3a
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2020-06-16 16:12:48 +0300

    getDeviceIDs - comments and params check

--- R/getConnections.R
+++ R/getConnections.R
@@ -1,40 +1,43 @@
-#' @title Builds a matrix object containing the IDs of the antennas to which devices are connected
+#' @title Builds a Matrix object containing the IDs of the antennas to which devices are connected.
+#'
+#' @description Builds a matrix object containing the IDs of the antennas to which devices are connected.
+#' The number of rows equals the number of devices and the number of columns equals the number of time instants
+#' when the network events were recorded. An element \code{[i,j]} in the returned matrix equals the ID of the antenna
+#' where the mobile device with index \code{i} in the ordered list of device IDs (returned by \code{getDeviceIDs()}) is
+#' conntected at the time instant with index \code{j} in the sequence of time instants when the network events
+#' where recorded.
+#'
+#' @param events A data.table object returned by \code{readEvents()} function.
+#'
+#' @return A matrix object with the antenna IDs where devices are connected for every time instants in the events file.
+#' If a device is not connected to any antenna at a time instant, the corresponding element in the matrix will have the
+#' value NA.
 #' 
-#' @description Builds a matrix object containing the IDs of the antennas to which devices are connected. 
-#' The number of rows equals the number of devices and the number of columns equals the number of time instants 
-#' when the network events were recorded. 
-#' An element \code{[i,j]} in the returned matrix equals the ID of the antenna where the mobile device with index \code{i} 
-#' in the input vector \code{devices} is conntected at the time instant with index \code{j} in the input vector \code{times}.
-#' 
-#' @param devices A vector with the devices IDs
-#' 
-#' @param events.dt A data.table object returned by \link{readEvents} function
-#' 
-#' @return A matrix object with the antenna IDs where devices are connected for every time instants in the event file. 
-#' If a device is not connected to any antenna at a time instant the corresponding element in the matrix will have the value NA.
-#' 
+#'
 #' @import data.table
-#' 
+#' @include getDeviceIDs.R
 #' @export
-getConnections <- function(events.dt) {
+getConnections <- function(events) {
+  times <- unique(events[, 1])
+  times <- unlist(times[order(times[, 1])])
   
-  times <- unique(events.dt[,1])
-  times <- unlist(times[order(times[,1])])
-  devices <- unique(events.dt[,2])
-  devices <- unlist(devices[order(devices[,1])])
+  devices <- getDeviceIDs(events)
+  devices <- unlist(devices[order(devices[, 1])])
   
   n1 <- length(times)
   n2 <- length(devices)
   
-  connections.dt <- matrix(ncol = n1, nrow = n2)
-
-  evv <- matrix(ncol = 2, nrow = n1)
-  evv[,1] <- times
-  colnames(evv)<-c('time', 'antennaID')
+  connections <- Matrix(-1L, ncol = n1, nrow = n2)
   
-  for(i in 1:n2){
-    evv <- merge(evv, events.dt[deviceID == devices[i],][,c(1,4)], by='time', all.x = TRUE)[,c(1,3)]
-    connections.dt[i,] <- t(evv[,2])
+  evv <- matrix(ncol = 2, nrow = n1)
+  evv[, 1] <- times
+  colnames(evv) <- c('time', 'antennaID')
+  
+  for (i in 1:n2) {
+    evv <-
+      merge(evv, events[deviceID == devices[i], ][, c(1, 4)], by = 'time', all.x = TRUE)[, c(1, 3)]
+    connections[i, ] <- t(evv[, 2])
   }
-  return (connections.dt)
+
+    return (connections)
 }
