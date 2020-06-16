2e91e5aa62f9daa0c46d735bf82b2f5b7450a2d2
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2020-06-16 15:59:32 +0300

    getJointEmissionProbs - comments and params check

--- R/getEmissionProbsJointModel.R
+++ R/getEmissionProbsJointModel.R
@@ -1,30 +1,36 @@
 #' @title Builds the emission probabilities for the joint HMM.
+#'
+#'
+#' @description Builds the emissions probabilities needed for the joint HMM used to estimate the posterior location
+#' probabilitities. 
+#'
+#' @param emissionProbs the emission probalbities (the location probabilities) computed by calling 
+#' \code{getEmissionProbs}.
 #' 
 #' 
-#' @description Builds the emissions probabilities needed for the joint HMM used to estimate the posterior location probabilitities. 
-#' In case of using simulated data, these probabilities are build using the signal strength or signal quality outputed
-#' by the simulation software for each tile in the grid.
-#' 
-#' @return Returns a matrix with the joint emission probabilities for the HMM. The number of rows equals the number of
-#' tiles in the grid and the number of columns equals the number of antennas plus one.
-#' 
-#' 
-#' @import data.table
-#' @include tileEquivalence.R
-#' 
+#' @return Returns a matrix with the joint emission probabilities for the HMM. The number of rows equals the
+#' number tiles and the number of columns equals the number of combinations between antenna IDs. Before the combination
+#' between antenna IDs are build, the NA value is added to the list of antenna IDs. An element in this matrix 
+#' represents the transition probability from an antenna to another, computed for each tile in the grid. 
+#'
+#'
+#' @import Matrix
+#'
 #' @export
 getEmissionProbsJointModel <- function(emissionProbs) {
   
-  emissionProbs <- cbind(emissionProbs, 'NA' = rep(1, nrow(emissionProbs)))
+  emissionProbs <-
+    cbind(emissionProbs, 'NA' = rep(1, nrow(emissionProbs)))
   jointEmissionProbs <- NULL
-  for(j in 1:ncol(emissionProbs)){
+  for (j in 1:ncol(emissionProbs)) {
     A <- emissionProbs[, j] * emissionProbs
-    colnames(A) <- paste0(colnames(emissionProbs)[j], "-", colnames(emissionProbs))
+    colnames(A) <-
+      paste0(colnames(emissionProbs)[j], "-", colnames(emissionProbs))
     jointEmissionProbs <- cbind(jointEmissionProbs, A)
   }
   colToRem <- ncol(jointEmissionProbs)
-  jointEmissionProbs <- jointEmissionProbs[, -colToRem]
+  jointEmissionProbs <- jointEmissionProbs[,-colToRem]
   
   return(jointEmissionProbs)
-
+  
 }
