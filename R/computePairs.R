9b922cf5a6a6df2cf106f6d777a0f2e766758c1a
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2020-06-17 13:40:08 +0300

    computePairs

--- R/computePairs.R
+++ R/computePairs.R
@@ -1,8 +1,47 @@
-
-
+#' @title Builds pairs of devices and corresponding connections
+#' 
+#' @description Build a data.table object that contains pairs of antenna IDs in the form "antennaID1-antennaID2", where antennaID1
+#' corresponds to a device while antennaID2 corresponds to another device, for each time instant over a time period when the 
+#' network events are registered. These pairs are build for each distinct combination "deviceID1-deviceID2" of devices. The rows of the
+#' data.table object corresponds to a combination of devices and the columns correponds to different time instants. The first two
+#' columns contains the device IDs of each pair af devices and the rest of the columns correspond to a time instant and contains
+#' the pairs antennaID1-antennaID2 where the two devices are connected at that time instant.
+#' 
+#' @param connections A matrix wuth the antenna ID where the mobile devices are connected at every time instant. Each row corresponds
+#' to a device and each column to a time instant. This matrix is obtained by calling \code {getConnections()} function.
+#' 
+#' @param ndevices The number of devices registered by the network. A vector with device IDs can be obtained by calling 
+#' \code{getDevices()} function and the number of devices is simply the lenght of this vector.
+#' 
+#' @param oneToOne If TRUE, the result is build to apply the method "1to1" to compute the duplicity probability for each device.
+#' This means that the result will contain all combinations of devices. If FALSE, the result will consider the proximity of antennas
+#' through the parameter antennaNeighbors and remove all the combinations of devices that are impossible to belong to a single person.
+#' If most of the time instants two devices are connected to two antennas that are not neighbours (i.e. their cells don't overlap)
+#' we consider that these two devices belong to different person and remove this combination of devices form the data.table result. 
+#' The term "most of the time instants" is implemented like this: we add how many times during the time horizon two devices are
+#' connected to neighbouring antennas and than keep in the final result only those combinations of devices with this number
+#' greater than the quantile of the sequence 0...(Number of time instants) with probability 1-P1-limit. 
+#' In this way we reduce the time complexity of the duplicity probability computation.
+#' 
+#' @param P1 The apriori probability of duplicity. It is obtained by calling \code{aprioriDuplicityProb()} function.
+#'
+#' @param limit A number that stands for the error in computing  apriori probability of duplicity.
+#'
+#' @param antennaNeighbors A data.table object with a single column 'nei' that contains pairs of antenna IDs in the form
+#' antennaID1-antennaID2 that are neighbours. We consider that two antennas are neighbours if their coverage areas has a 
+#' non void intersection.
+#' 
+#' @return 
+#' 
+#' @import data.table
 #' @export
-computePairs <- function(connections, ndevices, antennaNeighbors, P1, limit, one = FALSE) {
-
+computePairs <- function(connections, ndevices, oneToOne = TRUE, P1 = 0, limit = 0.05, antennaNeighbors = NULL) {
+  
+  if(oneToOne == TRUE & is.null(antennaNeighbors))
+    stop("To apply \"pairs\" method you need to provide antennaNeighbors parameter")
+  
+  pairs4duplicity <- NULL
+  
   connections[is.na(connections)] <- "NA"
   connections <- data.table(connections)[, index := .I]
   
@@ -10,10 +49,15 @@ computePairs <- function(connections, ndevices, antennaNeighbors, P1, limit, one
   allPairs <- data.table(t(combn(c(1:ndevices), 2)))
   setnames(allPairs, c("index.x", "index.y"))
   
+  #antennaId where the first device is connected
   allPairs_connections.dt1 <- merge(allPairs, connections , by.x = c("index.x"),
                                     by.y = c("index"), all.x = TRUE)
+  
+  #antennaId where the second device is connected
   allPairs_connections.dt2 <- merge(allPairs, connections , by.x = c("index.y"),
                                     by.y = c("index"), all.x = TRUE)
+  
+  #change the column names to allow rbindlist
   setcolorder(allPairs_connections.dt2, names(allPairs_connections.dt1))
   allPairs_connections.dt <- rbindlist(list(allPairs_connections.dt1, allPairs_connections.dt2))
   
@@ -21,26 +65,19 @@ computePairs <- function(connections, ndevices, antennaNeighbors, P1, limit, one
   rm(allPairs_connections.dt1)
   rm(allPairs_connections.dt2)
   
-  allPairs_connections_concat.dt <- allPairs_connections.dt[, lapply(.SD, paste0, collapse="-"), by = c("index.x", "index.y")]
-  
-  allPairs_connections_concat.dt[allPairs_connections_concat.dt == "NA-NA"] <- NA
-  if(!one) {
-    keepCols <- names(allPairs_connections_concat.dt)[-which(names(allPairs_connections_concat.dt) %in% c("index.x", "index.y"))]
-    
-    number <- sapply(allPairs_connections_concat.dt[, ..keepCols],function(x){ x %in% antennaNeighbors[,nei]})
-    
-    allPairs_connections_concat.dt[, number := apply(number, 1, sum)]
+  pairs4duplicity <- allPairs_connections.dt[, lapply(.SD, paste0, collapse="-"), by = c("index.x", "index.y")]
+  pairs4duplicity[pairs4duplicity == "NA-NA"] <- NA
+
+  if(oneToOne == FALSE) {
+    keepCols <- names(pairs4duplicity)[-which(names(pairs4duplicity) %in% c("index.x", "index.y"))]
+    number <- sapply(pairs4duplicity[, ..keepCols],function(x){ x %in% antennaNeighbors[,nei]})
+    pairs4duplicity[, number := apply(number, 1, sum)]
     rm(number)
     
-    allPairs_connections_concat.dt[, num_NA := Reduce(`+`, lapply(.SD,function(x) is.na(x))), .SDcols = keepCols]
-    allPairs_connections_concat.dt[, final_num := number + num_NA]
-    
-    
-    pairs4duplicity <- copy(allPairs_connections_concat.dt)[final_num >= quantile(c(0:length(keepCols)), probs = 1-P1-limit)][, c( "index.x", "index.y", keepCols), with = FALSE]
-    rm(allPairs_connections_concat.dt)   
-    return (pairs4duplicity)
-  } else {
-    return (allPairs_connections_concat.dt)
-  }
-  
+    pairs4duplicity[, num_NA := Reduce(`+`, lapply(.SD,function(x) is.na(x))), .SDcols = keepCols]
+    pairs4duplicity[, final_num := number + num_NA]
+
+    pairs4duplicity <- pairs4duplicity[final_num >= quantile(c(0:length(keepCols)), probs = 1-P1-limit)][, c( "index.x", "index.y", keepCols), with = FALSE]
+  } 
+  return (pairs4duplicity)
 }
