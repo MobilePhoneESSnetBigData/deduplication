fdb7e1ea63466f60a0cfa0fb8ba26883422e93eb
bogdan.oancea@gmail.com <bogdan.oancea@gmail.com>
2020-06-17 13:50:41 +0300

    aprioriOneDevice

--- R/aprioriOneDevice.R
+++ R/aprioriOneDevice.R
@@ -1,8 +1,17 @@
-
-
+#' @title Apriori probability of a person to have one mobile device.
+#'
+#' @description Apriori probability of a person to have one mobile device. It is computed simply as (2*Ndev1-Ndev)/Ndev1 where
+#' Ndev1 is the number of devices that are in a 1-to-1 correspondence with the persons that hold them.
+#'
+#'@param prob2Devices The probability of a person to have 2 devices. In case of using simulated data, this parameter is read
+#'from the simulation configuration file.
+#'
+#'@param ndevices The number of devices detected by the network during the time horizon under consideration.
+#'
 #' @export
 aprioriOneDeviceProb <- function(prob2Devices, ndevices) {
-  n_ext <- round((1 - prob2Devices) * ndevices)  
-  Pii <- (2*n_ext - ndevices)/(n_ext)  # priori probability of 1:1
+  n_ext <- round((1 - prob2Devices) * ndevices)
+  Pii <-
+    (2 * n_ext - ndevices) / (n_ext)  # priori probability of 1:1
   return(Pii)
 }
