f71ecce383317c3d929f1df7cee03b90d013d2c4
Bogdan Oancea <bogdan.oancea@gmail.com>
2020-06-19 17:06:20 +0300

    example file

--- R/example1.R
+++ R/example1.R
@@ -1,6 +1,6 @@
-#' @title Example
+#' Example of using deduplication package - the simple way
 #' 
-#' @description 
+
 #' This is just an example on how to compute duplicity probabilities using simulated data. All the files used in this
 #' example are supposed to be produced using the simulation software. The "simulation.xml" file is an exeception and it
 #' is an input file for the simulation software. The files used in this example are provided with the deduplication
