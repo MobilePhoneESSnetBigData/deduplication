f563465bf21f9e35b1acce634ed4b735562f98f8
Bogdan Oancea <bogdan.oancea@gmail.com>
2020-06-28 12:22:59 +0300

    buildDeltaProb

--- R/buildDeltaProb.R
+++ R/buildDeltaProb.R
@@ -22,7 +22,25 @@ buildDeltaProb<-function(centroids, device1, device2, path, dim, t) {
   if( dim!=1 & dim!=2)
     stop('dim should be 1 or 2')
   
-  posLoc1 <- readPostLocProb(path, device1)[,t]
-  posLoc1 <- readPostLocProb(path, device2)[,t]
-  
+  posLoc1 <- readPostLocProb(path, device1)[,..t]
+  posLoc2 <- readPostLocProb(path, device2)[,..t]
+  if( dim == 1) {
+    x<-unique(centroids$x)
+    deltaX <- as.data.table(expand.grid(x,x))
+    deltaX[,p1:=0]
+    deltaX[,p2:=0]
+    colnames(deltaX) <-c('x1', 'x2', 'p1', 'p2')
+    pp1<-vector(length = nrow(deltaX))
+    pp2<-vector(length = nrow(deltaX))
+    for(i in 1:length(x)) {
+      xx<-x[i]
+      tiles1<-centroids[,x]==xx
+      tiles2<-centroids[,x]==xx
+      pp1[deltaX[,x1]==xx]<-sum(posLoc1[tiles1,])
+      pp2[deltaX[,x2]==xx]<-sum(posLoc2[tiles2,])
+    }
+    deltaX[,p1:=pp1]
+    deltaX[,p2:=pp2]
+  }
+  return(deltaX)
 }
