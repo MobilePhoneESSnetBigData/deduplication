779952bd92a59ec47daaf6b8ddda3ea08d98594b
Bogdan Oancea <9313914+bogdanoancea@users.noreply.github.com>
2020-07-14 14:28:54 +0300

    Delete buildDelta.R

--- R/buildDelta.R
+++
@@ -1,9 +0,0 @@
-buildDelta <- function(centroids, dim) {
-  
-  x <- unique(centroids[, ..dim][[1]])
-  deltaX <- as.data.table(expand.grid(x, x))
-  deltaX[, dx := Var1 - Var2]
-  colnames(deltaX) <- c('x1', 'x2', 'dx')
-  return (deltaX)
-  
-}
